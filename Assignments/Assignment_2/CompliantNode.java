fb4c76c2b2e28639438d4b5ebce529ec845edc9f
Clotonervo <sam.hopkins@blastsoccer.net>
2020-03-02 18:04:26 -0700

    Assignment 2 stuff

--- Assignments/Assignment_2/CompliantNode.java
+++ Assignments/Assignment_2/CompliantNode.java
@@ -1,26 +1,57 @@
-import java.util.ArrayList;
-import java.util.Set;
+
+import java.util.*;
+import java.util.stream.Collectors;
+// This code gets 90/100
+//https://www.coursera.org/learn/cryptocurrency/programming/Bn6DI/consensus-from-trust/discussions/threads/VKiamAWOEee-uQ7R_UJsXg
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+    private double p_graph;
+    private double p_malicious;
+    private double p_txDistribution;
+    private int numRounds;
 
+    private boolean[] followees;
+    private Set<Transaction> pendingTransactions;
+    private boolean[] maliciousList;
+
+    // Constructor for CompliantNode
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = followees;
+        this.maliciousList = new boolean[followees.length];
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        this.pendingTransactions = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        Set<Transaction> sendTransactions = new HashSet<>(pendingTransactions);
+        pendingTransactions.clear();
+        return sendTransactions;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+        Set<Integer>sendingNodes = candidates.stream().map(candidate -> candidate.sender).collect(Collectors.toSet());
+
+        for (int i = 0; i < followees.length; i++) {
+            if(followees[i] && !sendingNodes.contains(i)){
+                maliciousList[i] = true;
+            }
+        }
+
+        for (Candidate candidate: candidates) {
+            if(!maliciousList[candidate.sender]){
+                pendingTransactions.add(candidate.tx);
+            }
+        }
     }
+
 }
